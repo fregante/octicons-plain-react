d67e4e1a9aa59df26c5defd153804e00038cd685
Federico Brigante <me@fregante.com>
2024-06-24 13:18:32 +0800

    Fix linux compat

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -8,7 +8,7 @@ mkdir components svgs
 for file in node_modules/@primer/octicons/build/svg/*-16.svg; do
 	NAME=$(basename "$file" -16.svg)
 	cp "$file" "svgs/$NAME.svg"
-	sed -i "" "s/xmlns/class=\"octicon octicon-$NAME\" xmlns/g" "svgs/$NAME.svg"
+	sed -i "s/xmlns/class=\"octicon octicon-$NAME\" xmlns/g" "svgs/$NAME.svg"
 
 	# assert that the file create includes the word octicon
 	grep -q octicon "svgs/$NAME.svg"
